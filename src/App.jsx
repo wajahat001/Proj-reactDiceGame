007cbcd18989aa78d67f45f3e6d77a0e6199d4c6
wajahat001 <wajahatchaudy1640@gmail.com>
2024-12-03 18:51:31 +0500

    First commit of basic template

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  padding: 10px;
+`;
+
+function App() {
+  return (
+    <>
+      <Button> This is B</Button>
+    </>
+  );
+}
+
+export default App;
