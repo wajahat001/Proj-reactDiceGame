84eb3eedab73187c2760f01d365fac183480811d
wajahat001 <wajahatchaudy1640@gmail.com>
2024-12-11 08:45:35 +0500

    working on mainPage.css

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import './mainPage.css'
 
 const MainPage = () => {
-  return (
-    <div></div>
-  )
+  return <div>
+    <div className='header'>
+      <div>
+          <h3>Total Score</h3>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+
+
+
+  </div>
+  
 }
 
 export default MainPage
